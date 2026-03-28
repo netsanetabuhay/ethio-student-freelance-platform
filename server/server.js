1e6f63f888ca28f1c7a99f32ea7375c4b211b9da
Netsanet Abuhay <netsanetabuhay@gmail.com>
2026-03-29 00:49:45 +0300

    after the code reabit is working fine

--- server/server.js
+++ server/server.js
@@ -2,32 +2,22 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-// Import DB connection
 const { connectDB } = require('./config/db');
-
-console.log("🚀 Starting server setup...");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-console.log("📦 Middlewares loading...");
-
 app.use(cors());
 app.use(express.json());
 
-var x = 10
-if(x == "10"){
-    console.log("bad comparison")
-}
-
+// Connect DB
+connectDB();
 
 // Test route
 app.get('/', (req, res) => {
-    console.log("🌐 GET / request received");
     res.send('Server is running...');
 });
 
 app.listen(PORT, () => {
-    console.log(`🔥 Server running on port ${PORT}`);
-    console.log(`👉 http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
